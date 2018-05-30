1952ca12294372e493fa144abdcfe4e83d39a80d
Jordan Couperthwaite@ <jbcouperthwaite@gmail.com>
2018-05-30 17:12:39 +0200

    Added account Form
    Added account form box

--- ChatMessageSystem/ChatMessageSystem/CreateAccount.Designer.cs
+++ ChatMessageSystem/ChatMessageSystem/CreateAccount.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ChatMessageSystem
 {
-    partial class ChatBoxForm
+    partial class CreateAccount
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,11 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.SuspendLayout();
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Items.AddRange(new object[] {
-            "Jessica",
-            "Jordan",
-            "Bob"});
-            this.listBox1.Location = new System.Drawing.Point(42, 54);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(120, 95);
-            this.listBox1.TabIndex = 0;
-            // 
-            // ChatBoxForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(703, 295);
-            this.Controls.Add(this.listBox1);
-            this.Name = "ChatBoxForm";
-            this.Text = "ChatBoxForm";
-            this.ResumeLayout(false);
-
+            this.Text = "CreateAccount";
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listBox1;
     }
 }
