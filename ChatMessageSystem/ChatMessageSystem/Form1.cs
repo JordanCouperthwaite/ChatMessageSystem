df04124fcfdcc08b7efce170e34bb1b2369103fd
Jordan Couperthwaite@ <jbcouperthwaite@gmail.com>
2018-05-30 15:44:25 +0200

    ChatMessageSystem

---
+++ ChatMessageSystem/ChatMessageSystem/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatMessageSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnPressExit_Click(object sender, EventArgs e)
+        {
+            btnPressExit.Text = "I love u";
+        }
+    }
+}
