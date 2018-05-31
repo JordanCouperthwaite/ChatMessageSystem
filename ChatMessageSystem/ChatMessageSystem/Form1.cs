b9a1ba4fb7298e04a271e18ff2aa5b94b288a22c
Jordan Couperthwaite@ <jbcouperthwaite@gmail.com>
2018-05-31 15:47:13 +0200

    Added comments
    added some comments on the login form explaining the connection.

--- ChatMessageSystem/ChatMessageSystem/Form1.cs
+++ ChatMessageSystem/ChatMessageSystem/Form1.cs
@@ -18,10 +18,10 @@ namespace ChatMessageSystem
             InitializeComponent();
         }
 
-
+        
         private void btnPressExit_Click(object sender, EventArgs e)
         {
-      
+            //user prompt to exit the program.
             var result = MessageBox.Show("Are you sure you would like to exit?",  "ChatMessageSystem-Exit", MessageBoxButtons.YesNo);
             
             if (result == System.Windows.Forms.DialogResult.Yes)
@@ -32,7 +32,10 @@ namespace ChatMessageSystem
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
+            //SQL connection created.
             SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Wolf\Documents\ChatMessageSystemDB.mdf;Integrated Security=True;Connect Timeout=30");
+            /*Selects all the instances where the specified Username and Password are input, there will only be one as Username is unique.
+             * If an instance is found, count will equal 1 which fufills the if statement and confirms Authentication to the server. */
             SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) From LoginTable where UserName = '" + txtBoxUsername.Text + "' and Password = '" + txtBoxPassword.Text + "'", con);
             DataTable dt = new DataTable();
             sda.Fill(dt);
